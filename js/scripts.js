572aa2a054d6eb8a060bb92c5a6b8cec52a3d2ba
Rehan Edin <rehanedin@gmail.com>
2018-05-11 15:46:26 +0300

    add the Javascript math function to genrate random numbers

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//Userinterface logic//
+$(document).ready(function(){
+    (#p1-roll).click(function(event) {
+        p1= new player;
+        p2= new player;
+
+        
+        event.preventDefault;
+    });
+});
+
+//Business logic//
+var start= function() {
+1+Math.floor(Math.random() * 6)
+};
