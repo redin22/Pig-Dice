351b06b34b61ae43e2a3664d9f919e9816e74cb0
Rehan Edin <rehanedin@gmail.com>
2018-05-12 19:18:10 +0300

    add player icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
+//Business logic
+/*var player1="";
+var player2="";
 
-
-//Business logic//
-var start= function() {
-1+ Math.floor(Math.random() *6);
+var die1= function() {
+ return Math.floor(Math.random() *6) +1;
 };
-console.log(You rolled "+start");
-var score=[];
-score=start;
+
+if(start1 === 1 || start2 == 1) { 
+    return 0;
+    }else {return score}
+ 
+var Game = {
+    player1Score: 0,
+    player2Score: 0,
+    result: 0,
+    };
+    var pRoll = function() {
+        var roll = dieRoll();
+        if(roll ===1){
+          Game.result = 0;
+          alertEndTurn();
+          switchPlayer();
+        } else {
+          Game.result +=roll;  
+          return roll;*/
+
+//userinterface logic
+$(document).ready(function(){
+    $(#p1-roll).click(function(){
+        return Math.floor(Math.random() *6) +1;
+    })
+});
+
 
